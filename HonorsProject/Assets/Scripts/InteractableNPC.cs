4c584c66ac5275a0080b1b9e2a48d1063b433b37
Lee Simpkin <lsimpkin710@gmail.com>
2026-02-17 15:40:51 +0000

    fixed the bug

--- HonorsProject/Assets/Scripts/InteractableNPC.cs
+++ HonorsProject/Assets/Scripts/InteractableNPC.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class InteractableNPC : MonoBehaviour
@@ -11,9 +14,11 @@ public class InteractableNPC : MonoBehaviour
     public TMPro.TextMeshProUGUI dialogueText;
     private TextFileManager loadInTextFile;
     public TextFileManager TFM;
-    public TextAsset textFilePath;
+    public TextAsset NPCDialogue;
     public TextAsset playerInput;
     private string inputText;
+    private bool wasInRange = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,21 +29,33 @@ public class InteractableNPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(playerPosition.position, NPCPosition.position) < 2f)
+        dialogueText.SetText(NPCDialogue.text);
+        bool isInRange = Vector3.Distance(playerPosition.position, NPCPosition.position) < 2f;
+        
+        if (isInRange)
         {
             interactionPrompt.SetText("Press E to interact");
             if (Input.GetKeyDown(KeyCode.E))
             {
                 Debug.Log("Interacted with NPC");
                 inputText = playerInput.text;
-                System.Diagnostics.Process.Start("CMD.exe", $"ollama run llama3.2 {inputText}");
-                string text = TFM.LoadText(textFilePath);
-                dialogueText.SetText(text);
+                System.Diagnostics.Process.Start("CMD.exe", "/C ollama run PriateLLM hello > \"Assets\\NPCLLMTool\\NPC\\NPCDialogue.txt\"");
+                
             }
+            wasInRange = true;
         }
         else
         {
             interactionPrompt.SetText("");
+            dialogueText.SetText("");
+            
+            // Clear the file when player leaves range
+            if (wasInRange)
+            {
+                string filePath = Path.Combine(Application.dataPath, "NPCLLMTool\\NPC\\NPCDialogue.txt");
+                File.WriteAllText(filePath, string.Empty);
+                wasInRange = false;
+            }
         }
     }
 
